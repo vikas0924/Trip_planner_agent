f2418ed75654a98a54d356a2a5a1a9c20f8f65cb
Vikas <sinha.v1996@gmail.com>
2026-09-24 12:35:52 +0530

    app added

--- test.py
+++ test.py
@@ -1,10 +1,13 @@
 from tools.tavily_tool import tavily_search
 from tools.flight_tool import search_flights
-
+from backend import run_travel_agent
 
 
 # res = tavily_search("Best hotels in Nagpur")
 # print(res)
 
-res = search_flights("Plan a 7 days Rajasthan trip from Delhi")
+# res = search_flights("Plan a 7 days Rajasthan trip from Delhi")
+# print(res)
+
+res = run_travel_agent('Plan a 2 days trip from india to japan', 'test_user')
 print(res)
